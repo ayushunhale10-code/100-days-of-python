9c2dcecdb524b127b85732f91170670d78a3c0db
ayushunhale10-code <ayushunhale10@gmail.com>
2026-02-03 18:49:14 +0530

    day 6 python practice

---
+++ Day-06/6.3 Exercise.py
@@ -0,0 +1,26 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def upside():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while at_goal != true:
+    if front_is_clear():
+        move()
+    else:
+        upside()
+
+
+
+
+
+
+
